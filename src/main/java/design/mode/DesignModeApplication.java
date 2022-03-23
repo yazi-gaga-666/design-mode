aa0cb43d29daf21377deda3d02fdcd18199018aa
yazi-gaga-666 <imcc0924@163.com>
2022-03-23 16:24:04 +0800

    初始化

---
+++ src/main/java/design/mode/DesignModeApplication.java
@@ -0,0 +1,14 @@
+package design.mode;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class,args);
+    }
+
+}
